c16cc3bbdbf019738b763cda18d9e2aff11de151
Josh1289op <Joshuakegley@gmail.com>
2015-01-14 18:18:47 -0500

    working translate screen

--- src/com/joshuakegley/panels/TranslateInterface.java
+++ src/com/joshuakegley/panels/TranslateInterface.java
@@ -5,6 +5,16 @@
  */
 package com.joshuakegley.panels;
 
+import com.joshuakegley.hcitranslation.MainDriver;
+import com.joshuakegley.hcitranslation.Settings;
+import com.translatorService.Translate;
+import java.awt.EventQueue;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JButton;
+import org.json.simple.parser.ParseException;
+
 /**
  *
  * @author Joshua
@@ -15,19 +25,96 @@ public class TranslateInterface extends javax.swing.JPanel {
      * Creates new form TranslateInterface
      */
     public TranslateInterface() {
+
         initComponents();
+        
+        lang1TranslateBtn.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                actionListnerTranslate(evt);
+            }
+        });
+        lang2TranslateBtn.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                actionListnerTranslate(evt);
+            }
+        });
+        
     }
 
-    public static void setLang1(String text){
-        System.out.println("TEST");
-        lang1Label.setText(text);
+    public static void setLang1(String langLabel, String langCode){
+            lang1Label.setText(langLabel);
+            if(langCode == "en"){
+                lang1TranslateBtn.setText("Translate");
+                lang1ResetBtn.setText("Reset");
+            }else{
+                EventQueue.invokeLater(new Runnable() {
+                    public void run() {
+                        try {
+                            lang1TranslateBtn.setText(Translate.translate("Translate", "en", langCode));
+                            lang1ResetBtn.setText(Translate.translate("Reset", "en", langCode));
+                        } catch (ParseException ex) {
+                            Logger.getLogger(TranslateInterface.class.getName()).log(Level.SEVERE, null, ex);
+                        } catch (IOException ex) {
+                            Logger.getLogger(TranslateInterface.class.getName()).log(Level.SEVERE, null, ex);
+                        }
+                    }
+                });
+            }
     }
     
-    public static void setLang2(String text){
-        lang2Label.setText(text);
+    public static void setLang2(String langLabel, String langCode){
+        lang2Label.setText(langLabel);
+        if(langCode == "en"){
+            lang2TranslateBtn.setText("Translate");
+            lang2ResetBtn.setText("Reset");
+            return;
+        }else{
+            EventQueue.invokeLater(new Runnable() {
+                public void run() {
+                    try {
+                        lang2TranslateBtn.setText(Translate.translate("Translate", "en", langCode));
+                        lang2ResetBtn.setText(Translate.translate("Reset", "en", langCode));
+                    } catch (ParseException ex) {
+                        Logger.getLogger(TranslateInterface.class.getName()).log(Level.SEVERE, null, ex);
+                    } catch (IOException ex) {
+                        Logger.getLogger(TranslateInterface.class.getName()).log(Level.SEVERE, null, ex);
+                    }
+                }
+            });
+        }
     }    
     
-    
+    private void actionListnerTranslate(java.awt.event.ActionEvent evt) {                                             
+        int user = Integer.parseInt(((JButton)evt.getSource()).getName());
+        
+        if (user == 1){
+            String message = lang1Text.getText();
+            try {
+                String result = Translate.translate(message, Settings.getLang1(), Settings.getLang2());
+                result = result.replaceAll("&#39;","\'");
+                displayPane.append("\n" + result);            
+            } catch (ParseException ex) {
+                Logger.getLogger(TranslateInterface.class.getName()).log(Level.SEVERE, null, ex);
+            } catch (IOException ex) {
+                Logger.getLogger(TranslateInterface.class.getName()).log(Level.SEVERE, null, ex);
+            }
+            
+        }else {
+            String message = lang2Text.getText();
+            try {
+                String result = Translate.translate(message, Settings.getLang2(), Settings.getLang1());
+                result = result.replaceAll("&#39;","\'");
+                displayPane.append("\n" + result);
+
+            } catch (ParseException ex) {
+                Logger.getLogger(TranslateInterface.class.getName()).log(Level.SEVERE, null, ex);
+            } catch (IOException ex) {
+                Logger.getLogger(TranslateInterface.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+        
+        
+    } 
     
     /**
      * This method is called from within the constructor to initialize the form.
@@ -41,9 +128,9 @@ public class TranslateInterface extends javax.swing.JPanel {
         jScrollPane1 = new javax.swing.JScrollPane();
         displayPane = new javax.swing.JTextArea();
         lang1TranslateBox = new javax.swing.JScrollPane();
-        jTextArea2 = new javax.swing.JTextArea();
+        lang1Text = new javax.swing.JTextArea();
         lang2TranslateBox = new javax.swing.JScrollPane();
-        jTextArea3 = new javax.swing.JTextArea();
+        lang2Text = new javax.swing.JTextArea();
         lang2TranslateBtn = new javax.swing.JButton();
         lang1TranslateBtn = new javax.swing.JButton();
         lang1ResetBtn = new javax.swing.JButton();
@@ -54,33 +141,38 @@ public class TranslateInterface extends javax.swing.JPanel {
         setPreferredSize(new java.awt.Dimension(586, 469));
 
         displayPane.setEditable(false);
+        displayPane.setBackground(new java.awt.Color(222, 220, 220));
         displayPane.setColumns(20);
+        displayPane.setLineWrap(true);
         displayPane.setRows(5);
+        displayPane.setWrapStyleWord(true);
         jScrollPane1.setViewportView(displayPane);
 
-        jTextArea2.setColumns(20);
-        jTextArea2.setRows(5);
-        lang1TranslateBox.setViewportView(jTextArea2);
+        lang1Text.setBackground(new java.awt.Color(255, 204, 204));
+        lang1Text.setColumns(20);
+        lang1Text.setLineWrap(true);
+        lang1Text.setRows(5);
+        lang1TranslateBox.setViewportView(lang1Text);
 
-        jTextArea3.setColumns(20);
-        jTextArea3.setRows(5);
-        lang2TranslateBox.setViewportView(jTextArea3);
+        lang2Text.setBackground(new java.awt.Color(204, 204, 255));
+        lang2Text.setColumns(20);
+        lang2Text.setLineWrap(true);
+        lang2Text.setRows(5);
+        lang2Text.setWrapStyleWord(true);
+        lang2TranslateBox.setViewportView(lang2Text);
 
-        lang2TranslateBtn.setFont(new java.awt.Font("Quicksand", 0, 12)); // NOI18N
+        lang2TranslateBtn.setFont(new java.awt.Font("Arial Unicode MS", 0, 12)); // NOI18N
         lang2TranslateBtn.setText("Translate");
+        lang2TranslateBtn.setName("2"); // NOI18N
 
-        lang1TranslateBtn.setFont(new java.awt.Font("Quicksand", 0, 12)); // NOI18N
+        lang1TranslateBtn.setFont(new java.awt.Font("Arial Unicode MS", 0, 12)); // NOI18N
         lang1TranslateBtn.setText("Translate");
-        lang1TranslateBtn.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                lang1TranslateBtnActionPerformed(evt);
-            }
-        });
+        lang1TranslateBtn.setName("1"); // NOI18N
 
-        lang1ResetBtn.setFont(new java.awt.Font("Quicksand", 0, 12)); // NOI18N
+        lang1ResetBtn.setFont(new java.awt.Font("Arial Unicode MS", 0, 12)); // NOI18N
         lang1ResetBtn.setText("Reset");
 
-        lang2ResetBtn.setFont(new java.awt.Font("Quicksand", 0, 12)); // NOI18N
+        lang2ResetBtn.setFont(new java.awt.Font("Arial Unicode MS", 0, 12)); // NOI18N
         lang2ResetBtn.setText("Reset");
 
         lang1Label.setFont(new java.awt.Font("Quicksand", 2, 24)); // NOI18N
@@ -120,7 +212,7 @@ public class TranslateInterface extends javax.swing.JPanel {
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addContainerGap(21, Short.MAX_VALUE)
+                .addContainerGap(17, Short.MAX_VALUE)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addComponent(lang1Label, javax.swing.GroupLayout.Alignment.TRAILING)
                     .addComponent(lang2Label, javax.swing.GroupLayout.Alignment.TRAILING))
@@ -144,23 +236,19 @@ public class TranslateInterface extends javax.swing.JPanel {
         );
     }// </editor-fold>//GEN-END:initComponents
 
-    private void lang1TranslateBtnActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_lang1TranslateBtnActionPerformed
-        // TODO add your handling code here:
-    }//GEN-LAST:event_lang1TranslateBtnActionPerformed
-
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JTextArea displayPane;
     private javax.swing.JScrollPane jScrollPane1;
-    private javax.swing.JTextArea jTextArea2;
-    private javax.swing.JTextArea jTextArea3;
     private static javax.swing.JLabel lang1Label;
-    private javax.swing.JButton lang1ResetBtn;
+    private static javax.swing.JButton lang1ResetBtn;
+    private javax.swing.JTextArea lang1Text;
     private javax.swing.JScrollPane lang1TranslateBox;
-    private javax.swing.JButton lang1TranslateBtn;
+    private static javax.swing.JButton lang1TranslateBtn;
     private static javax.swing.JLabel lang2Label;
-    private javax.swing.JButton lang2ResetBtn;
+    private static javax.swing.JButton lang2ResetBtn;
+    private javax.swing.JTextArea lang2Text;
     private javax.swing.JScrollPane lang2TranslateBox;
-    private javax.swing.JButton lang2TranslateBtn;
+    private static javax.swing.JButton lang2TranslateBtn;
     // End of variables declaration//GEN-END:variables
 }
